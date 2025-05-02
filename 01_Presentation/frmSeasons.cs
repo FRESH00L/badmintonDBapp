04af9d3814e0d993f3f96464fe25ad892b68b8e5
Victoria Rodríguez Álamo <victoriarodriguezalamo05@gmail.com>
2025-05-02 19:48:45 +0200

    Finish clearLabels()

--- 01_Presentation/frmSeasons.cs
+++ 01_Presentation/frmSeasons.cs
@@ -21,36 +21,7 @@ namespace BazyDanychBadminton._01_Presentation
         }
         private void frmSeason_Load(object sender, EventArgs e)
         {
-            q_first_player.Text = "";
-            q_second_player.Text = "";
-            q_third_player.Text = "";
-            q_fourth_player.Text = "";
-            q_fifth_player.Text = "";
-            q_sixth_player.Text = "";
-            q_seventh_player.Text = "";
-            q_eighth_player.Text = "";
-            s_first_player.Text = "";
-            s_second_player.Text = "";
-            s_third_player.Text = "";
-            s_fourth_player.Text = "";
-            f_first_player.Text = "";
-            f_second_player.Text = "";
-            winner_player.Text = "";
-            q_score_first_player.Text = "";
-            q_score_second_player.Text = "";
-            q_score_third_player.Text = "";
-            q_score_fourth_player.Text = "";
-            q_score_fifth_player.Text = "";
-            q_score_sixth_player.Text = "";
-            q_score_seventh_player.Text = "";
-            q_score_eighth_player.Text = "";
-            s_score_first_player.Text = "";
-            s_score_second_player.Text = "";
-            s_score_third_player.Text = "";
-            s_score_fourth_player.Text = "";
-            f_score_first_player.Text = "";
-            f_score_second_player.Text = "";
-            winner_score.Text = "";
+            clearLabels();
             season = new Season();
             List<int> list_seasons = season.ReadAllSeasons();
             foreach (int i in list_seasons)
@@ -76,36 +47,7 @@ namespace BazyDanychBadminton._01_Presentation
         {
             ClearSelectedList();
             lbx_ListOfSeasons.SelectedIndex = -1;
-            q_first_player.Text = "";
-            q_second_player.Text = "";
-            q_third_player.Text = "";
-            q_fourth_player.Text = "";
-            q_fifth_player.Text = "";
-            q_sixth_player.Text = "";
-            q_seventh_player.Text = "";
-            q_eighth_player.Text = "";
-            s_first_player.Text = "";
-            s_second_player.Text = "";
-            s_third_player.Text = "";
-            s_fourth_player.Text = "";
-            f_first_player.Text = "";
-            f_second_player.Text = "";
-            winner_player.Text = "";
-            q_score_first_player.Text = "";
-            q_score_second_player.Text = "";
-            q_score_third_player.Text = "";
-            q_score_fourth_player.Text = "";
-            q_score_fifth_player.Text = "";
-            q_score_sixth_player.Text = "";
-            q_score_seventh_player.Text = "";
-            q_score_eighth_player.Text = "";
-            s_score_first_player.Text = "";
-            s_score_second_player.Text = "";
-            s_score_third_player.Text = "";
-            s_score_fourth_player.Text = "";
-            f_score_first_player.Text = "";
-            f_score_second_player.Text = "";
-            winner_score.Text = "";
+            clearLabels();
         }
         private void ClearSelectedList()
         {
@@ -241,36 +183,7 @@ namespace BazyDanychBadminton._01_Presentation
         }
         private void lbx_ListOfSeasons_SelectedIndexChanged(object sender, EventArgs e)
         {
-            q_first_player.Text = "";
-            q_second_player.Text = "";
-            q_third_player.Text = "";
-            q_fourth_player.Text = "";
-            q_fifth_player.Text = "";
-            q_sixth_player.Text = "";
-            q_seventh_player.Text = "";
-            q_eighth_player.Text = "";
-            s_first_player.Text = "";
-            s_second_player.Text = "";
-            s_third_player.Text = "";
-            s_fourth_player.Text = "";
-            f_first_player.Text = "";
-            f_second_player.Text = "";
-            winner_player.Text = "";
-            q_score_first_player.Text = "";
-            q_score_second_player.Text = "";
-            q_score_third_player.Text = "";
-            q_score_fourth_player.Text = "";
-            q_score_fifth_player.Text = "";
-            q_score_sixth_player.Text = "";
-            q_score_seventh_player.Text = "";
-            q_score_eighth_player.Text = "";
-            s_score_first_player.Text = "";
-            s_score_second_player.Text = "";
-            s_score_third_player.Text = "";
-            s_score_fourth_player.Text = "";
-            f_score_first_player.Text = "";
-            f_score_second_player.Text = "";
-            winner_score.Text = "";
+            clearLabels();
             btn_DeleteSeason.Enabled = true;
             if (lbx_ListOfSeasons.SelectedItem != null)
             {
@@ -292,7 +205,6 @@ namespace BazyDanychBadminton._01_Presentation
         private void lbx_ListSelectedTournament_SelectedIndexChanged(object sender, EventArgs e)
         {
             clearLabels();
-
             if (lbx_ListSelectedTournament.SelectedItem != null && lbx_ListOfSeasons.SelectedItem != null)
             {
                 Tournament tournament = new Tournament();
